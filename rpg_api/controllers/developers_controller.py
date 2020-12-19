a9d4275d01c4311c54cef3c14bdf449c29a3f86c
Kloven <jloup.m@gmail.com>
2020-12-20 00:27:16 +0100

    Base

---
+++ rpg_api/controllers/developers_controller.py
@@ -0,0 +1,22 @@
+import connexion
+import six
+
+from rpg_api.models.inventory_item import InventoryItem  # noqa: E501
+from rpg_api import util
+
+
+def search_inventory(search_string=None, skip=None, limit=None):  # noqa: E501
+    """searches inventory
+
+    By passing in the appropriate options, you can search for available inventory in the system  # noqa: E501
+
+    :param search_string: pass an optional search string for looking up inventory
+    :type search_string: str
+    :param skip: number of records to skip for pagination
+    :type skip: int
+    :param limit: maximum number of records to return
+    :type limit: int
+
+    :rtype: List[InventoryItem]
+    """
+    return 'do some magic!'
---
+++ rpg_api/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from rpg_api.models.inventory_item import InventoryItem
+from rpg_api.models.manufacturer import Manufacturer
