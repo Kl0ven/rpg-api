35000e8c9548de57b13e75136fe915ae4f8ed74c
jean-loup.monnier <jloup.m@gmail.com>
2020-12-30 20:25:10 +0100

    Add rarety to loot

--- rpg_api/models/loot.py
+++ rpg_api/models/loot.py
@@ -5,12 +5,16 @@ from config import CONFIG
 class Loot(Item):
     type = peewee.IntegerField()
     image_url = peewee.TextField()
+    rarety = peewee.IntegerField()
 
     def __unicode__(self):
         return self.name
 
     def get_type_name(self):
         return CONFIG['invert_loot_type'][self.type]
+
+    def get_rarety_name(self):
+            return CONFIG['invert_lootboxes_rarety'][self.rarety]
 
     def to_dict(self):
         return {
@@ -19,8 +23,14 @@ class Loot(Item):
             "type": {
                 "value": self.type,
                 "name": self.get_type_name()
+            },
+            "rarety": {
+                "value": self.rarety,
+                "name": self.get_rarety_name()
             }
         }
+
+
 
     class Meta:
         db_table = "loots"
